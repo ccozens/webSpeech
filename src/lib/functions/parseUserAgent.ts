2148ed6c2348cabdf40215fc43e6e98334751800
ccozens <ccozens@gmail.com>
2023-09-06 11:18:02 +0100

    abstracted toLowerCase in parseUserAgent to own variable

--- src/lib/functions/parseUserAgent.ts
+++ src/lib/functions/parseUserAgent.ts
@@ -1,18 +1,23 @@
 import type { Device } from '$lib/types';
-export function parseUserAgent(userAgent: string): Device {
-  const deviceMap = {
-    android: { name: 'Android', type: 'mobile' },
-    iphone: { name: 'iPhone', type: 'mobile' },
-    ipad: { name: 'iPad', type: 'mobile' },
-    ipod: { name: 'iPod', type: 'mobile' },
-    'windows phone': { name: 'Windows Phone', type: 'mobile' },
-    windows: { name: 'Windows', type: 'desktop' },
-    macintosh: { name: 'Macintosh', type: 'desktop' },
-    linux: { name: 'Linux', type: 'desktop' },
-    cros: { name: 'Chrome OS', type: 'desktop' },
-    playstation: { name: 'PlayStation', type: 'console' },
-  };
 
-  const matchedDevice = Object.entries(deviceMap).find(([device]) => userAgent.toLowerCase().includes(device));
-  return matchedDevice ? { name: matchedDevice[1].name, type: matchedDevice[1].type } : { name: 'Unknown', type: 'unknown' };
+const deviceMap = {
+	android: { name: 'Android', type: 'mobile' },
+	iphone: { name: 'iPhone', type: 'mobile' },
+	ipad: { name: 'iPad', type: 'mobile' },
+	ipod: { name: 'iPod', type: 'mobile' },
+	'windows phone': { name: 'Windows Phone', type: 'mobile' },
+	windows: { name: 'Windows', type: 'desktop' },
+	macintosh: { name: 'Macintosh', type: 'desktop' },
+	linux: { name: 'Linux', type: 'desktop' },
+	cros: { name: 'Chrome OS', type: 'desktop' },
+	playstation: { name: 'PlayStation', type: 'console' }
+};
+export function parseUserAgent(userAgent: string): Device {
+	const lowerCaseUserAgent = userAgent.toLowerCase();
+	const matchedDevice = Object.entries(deviceMap).find(([device]) =>
+		lowerCaseUserAgent.includes(device)
+	);
+	return matchedDevice
+		? { name: matchedDevice[1].name, type: matchedDevice[1].type }
+		: { name: 'Unknown', type: 'other' };
 }
